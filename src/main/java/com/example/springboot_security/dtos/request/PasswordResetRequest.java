51fed745e42ad38b5cb1aeb41a5c6d882b91575b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-26 15:58:40 +0100

    passwordResetRequest updated

--- src/main/java/com/example/springboot_security/dtos/request/PasswordResetRequest.java
+++ src/main/java/com/example/springboot_security/dtos/request/PasswordResetRequest.java
@@ -1,2 +1,22 @@
-package com.example.springboot_security.dtos.request;public class PasswordResetRequest {
+package com.example.springboot_security.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetRequest {
+
+    @Email(message= "email is required")
+    private String email;
+
+    @NotBlank(message= "password cannot be blank")
+    private String password;
 }
