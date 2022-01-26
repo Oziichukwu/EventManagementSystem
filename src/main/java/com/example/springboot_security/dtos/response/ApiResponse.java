aab81f9510d4ac6965d70c85de2a43b642551019
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-26 16:00:32 +0100

    api response added

--- src/main/java/com/example/springboot_security/dtos/response/ApiResponse.java
+++ src/main/java/com/example/springboot_security/dtos/response/ApiResponse.java
@@ -1,2 +1,30 @@
-package com.example.springboot_security.dtos.response;public class ApiResponse {
+package com.example.springboot_security.dtos.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+    private boolean isSuccessful;
+
+    private String message;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime localDateTime;
+
+
+    public ApiResponse(boolean isSuccessful, String message) {
+        this.isSuccessful = isSuccessful;
+        this.message = message;
+        localDateTime = localDateTime.now();
+    }
+
+
 }
