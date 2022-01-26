4960da7fc2fceaec9063003a256c97955356796a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-26 05:26:41 +0100

    login functionality implemented

--- src/main/java/com/example/springboot_security/services/AuthServiceImpl.java
+++ src/main/java/com/example/springboot_security/services/AuthServiceImpl.java
@@ -8,10 +8,18 @@ import com.example.springboot_security.dtos.request.UserRequest;
 import com.example.springboot_security.dtos.response.JwtTokenResponse;
 import com.example.springboot_security.dtos.response.UserResponse;
 import com.example.springboot_security.exceptions.AuthException;
+import com.example.springboot_security.security.CustomUserDetailService;
+import com.example.springboot_security.security.JwtTokenProvider;
+import com.example.springboot_security.security.UserPrincipal;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -26,8 +34,17 @@ public class AuthServiceImpl implements AuthService{
     @Autowired
     private UserRepository userRepository;
 
-    //@Autowired
-    //private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
 
     @Override
@@ -38,7 +55,7 @@ public class AuthServiceImpl implements AuthService{
         }
 
         User user = modelMapper.map(userRequest, User.class);
-       // user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
          user.setVerificationCode(UUID.randomUUID().toString());
 
         User savedUser = save(user);
@@ -52,6 +69,20 @@ public class AuthServiceImpl implements AuthService{
 
     @Override
     public JwtTokenResponse login(LoginRequest loginRequest) {
-        return null;
+
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final UserPrincipal userDetails = (UserPrincipal) customUserDetailService.loadUserByUsername(loginRequest.getEmail());
+        final String token = jwtTokenProvider.generateToken(userDetails);
+        User user = internalDatabaseFindUserByEmail(loginRequest.getEmail());
+        return new JwtTokenResponse(token , user.getEmail());
+    }
+
+    private User internalDatabaseFindUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
     }
 }
