f12611f2240b27210ac02bc2714e4f99b3fc42e7
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-26 15:59:30 +0100

    auth service interface updated

--- src/main/java/com/example/springboot_security/services/AuthService.java
+++ src/main/java/com/example/springboot_security/services/AuthService.java
@@ -1,9 +1,14 @@
 package com.example.springboot_security.services;
 
+import com.example.springboot_security.data.models.Token;
 import com.example.springboot_security.dtos.request.LoginRequest;
+import com.example.springboot_security.dtos.request.PasswordRequest;
+import com.example.springboot_security.dtos.request.PasswordResetRequest;
 import com.example.springboot_security.dtos.request.UserRequest;
 import com.example.springboot_security.dtos.response.JwtTokenResponse;
 import com.example.springboot_security.dtos.response.UserResponse;
+import com.example.springboot_security.exceptions.AuthException;
+import com.example.springboot_security.exceptions.TokenException;
 
 public interface AuthService {
 
@@ -11,5 +16,10 @@ public interface AuthService {
 
     JwtTokenResponse login(LoginRequest loginRequest);
 
+    void updatePassword(PasswordRequest passwordRequest)throws AuthException, TokenException;
+
+    void resetPassword(PasswordResetRequest passwordResetRequest, String passwordResetToken)throws AuthException;
+
+    Token generatePasswordResetToken(String email)throws AuthException;
 
 }
